c520db149ae837148ead94c63725580fc27f6b96
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-03-01 10:24:22 -0300

    correção codigos dos livros

--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -35,14 +35,20 @@ export class ControleLivrosService {
     return this.livros
   }
   adicionar(livro:Livro){
-    livro.codigo =(this.livros.length -1) + 2;
-    this.livros.push(livro);
+    try{
+      var codigo:number =(this.livros.length -1);
+      livro.codigo = this.livros[codigo].codigo + 1;
+      this.livros.push(livro);
   }
-  excluir(num:Number){
-      this.livros.findIndex((temp:Livro, indice, livros)=>{
-        num == temp.codigo;
-        livros.splice(indice,1);
-        return livros
+    catch{
+      livro.codigo = 1;
+      this.livros.push(livro);
+    }
+  }
+  excluir(num:number){
+      var livroEncontrado = this.livros.findIndex((livro:Livro)=>{
+        return num == livro.codigo
       });
+      this.livros.splice(livroEncontrado, 1)
   }
 }
